976e9e944883858da9c7e6952a65708fced2254d
Facundo Venturi <venturifaku@gmail.com>
2024-02-29 12:04:06 -0300

    Ping Pong example

---
+++ common/src/lib.rs
@@ -0,0 +1,12 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+pub enum FromClientMessage {
+    Ping,
+}
+
+#[derive(Serialize, Deserialize)]
+pub enum FromServerMessage {
+    Pong(usize), // Used for connection oriented protocols
+    UnknownPong, // Used for non-connection oriented protocols
+}
